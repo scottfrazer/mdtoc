f366c836f602b3e9b44b28b22a7589c5c3458854
Scott Frazer <scott.d.frazer@gmail.com>
2015-12-10 10:12:29 -0500

    code

---
+++ README.md
@@ -0,0 +1,18 @@
+# Markdown TOC Generator
+
+Installable via `pip install mdtoc`
+
+Command-line utility to generate table of contents within Markdown files.
+
+Add these delimiters to your Markdown file:
+
+```
+<!---toc start-->
+<!---toc end-->
+```
+
+Then, run
+
+```
+mdtoc your_markdown.md
+```
---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+version = '1.0'
+long_description = 'Adds table of contents to Markdown files'
+
+setup(
+  name='mdtoc',
+  version=version,
+  description=long_description,
+  author='Scott Frazer',
+  author_email='scott.d.frazer@gmail.com',
+  packages=['mdtoc'],
+  install_requires=[
+      "xtermcolor",
+      "requests"
+  ],
+  scripts={
+      'scripts/mdtoc',
+  },
+  license = 'MIT',
+  keywords = "Markdown, table of contents, toc",
+  url = "http://github.com/scottfrazer/mdtoc",
+  classifiers=[
+      'License :: OSI Approved :: MIT License',
+      'Environment :: Console',
+      'Programming Language :: Python :: 2.7',
+      'Programming Language :: Python :: 3.4',
+      'Programming Language :: Python :: 3.5',
+      'Development Status :: 5 - Production/Stable',
+      'Intended Audience :: Developers',
+      'Natural Language :: English'
+  ]
+)
